0647bbd7f68864bee694d1bd80318c8eaa88840b
MehmetBulut94 <47386523+MehmetBulut94@users.noreply.github.com>
2020-04-12 02:04:34 +0300

    .

--- Home/forms.py
+++ Home/forms.py
@@ -6,3 +6,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={"placeholder": "İsim"}),
+            "email": forms.EmailInput(attrs={"palceholder": "Email adresiniz"}),
+            "message": forms.Textarea(attrs={"palceholder": "Mesajınız"}),
+
+        }
