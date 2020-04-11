4f4d6e7641ba1dcbc00226b1dabe314c6bf71f07
MehmetBulut94 <47386523+MehmetBulut94@users.noreply.github.com>
2020-04-11 23:16:35 +0300

    .

--- Home/views.py
+++ Home/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from .forms import ContactForm
 
 
+def index(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            form = ContactForm()
+    else:
+        form = ContactForm()
+
+    contex = {
+        "form": form
+    }
+    return render(request, 'home/index.html')
+
+
 def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
